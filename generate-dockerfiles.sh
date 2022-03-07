edc85de3f3a2280f3c4d28e9e11be072a40d1315
bfren <ben@bcgdesign.com>
2022-03-07 10:56:43 +0000

    v3.0.10 (#55)
    Minor updates
    * Using latest base images
    * Don't install full database on dev builds
    
    Build updates
    * Adding GitHub packages
    
    Development updates
    * Adding pull.sh and run/sh
    * Adding generate-dockerfiles.sh

---
+++ generate-dockerfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker pull bfren/alpine
+
+BASE_REVISION="4.1.1"
+echo "Base: ${BASE_REVISION}"
+
+CLAMAV_VERSIONS="0.103 0.104"
+for V in ${CLAMAV_VERSIONS} ; do
+
+    echo "ClamAV ${V}"
+    ALPINE_MINOR=`cat ./${V}/ALPINE_MINOR`
+
+    DOCKERFILE=$(docker run \
+        -v ${PWD}:/ws \
+        -e BF_DEBUG=0 \
+        bfren/alpine esh \
+        "/ws/Dockerfile.esh" \
+        BASE_REVISION=${BASE_REVISION} \
+        ALPINE_MINOR=${ALPINE_MINOR} \
+        CLAMAV_MINOR=${V}
+    )
+
+    echo "${DOCKERFILE}" > ./${V}/Dockerfile
+
+done
+
+docker system prune -f
+echo "Done."
